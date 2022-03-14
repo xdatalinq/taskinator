33a0cf281a693daada233bf4c6097fe1e860849c
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-03-13 22:32:22 -0400

    added form validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,11 @@ var taskFormHandler = function(event) {
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+  // check if input values are empty strings
+  if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
   // package up data as an object
   var taskDataObj = {
       name: taskNameInput,
@@ -14,6 +19,7 @@ var taskFormHandler = function(event) {
 
   // send it as an argument to createTaskEl
   createTaskEl(taskDataObj);
+  formEl.reset();
 };
 
 var createTaskEl = function (taskDataObj) {
